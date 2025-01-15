ef41a10e415765277c2569bd643e0bff07bfb636
im-ashar <muhammadashar401@gmail.com>
2025-01-16 00:02:53 +0500

    fxing some bugs and adding discount funtionality

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import {lucideCheck, lucideChevronDown, lucideChevronUp, lucideCirclePlus} from '@ng-icons/lucide';
+import {lucideCheck, lucideChevronDown, lucideChevronUp, lucideCirclePlus, lucideCircleX} from '@ng-icons/lucide';
 import {HlmButtonDirective} from '@spartan-ng/ui-button-helm';
 import {Component} from '@angular/core';
 import {
@@ -59,12 +59,13 @@ import {HlmSelectImports} from '@spartan-ng/ui-select-helm';
     HlmSelectImports,
     BrnDialogContentDirective,
   ],
-  providers: [provideIcons({lucideCheck, lucideChevronDown, lucideChevronUp, lucideCirclePlus})],
+  providers: [provideIcons({lucideCheck, lucideChevronDown, lucideChevronUp, lucideCirclePlus, lucideCircleX})],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
   persons: Person[] = [];
+  inputPersons: Person[] = [];
   numberOfPersons: number | undefined;
   shouldShowBillCards: boolean = false;
   shouldShowInputs: boolean = false;
@@ -73,6 +74,8 @@ export class HomeComponent {
   totalBill: number = 0;
   selectedPersons: Person[] = [];
   itemPrice: number | undefined;
+  itemDiscount: number | undefined;
+  discountOnTotalBill: number | undefined;
 
   generateNameInput(): void {
     if (this.numberOfPersons === undefined || this.numberOfPersons <= 1) {
@@ -80,39 +83,44 @@ export class HomeComponent {
       return;
     }
 
-    if (this.numberOfPersons && this.numberOfPersons > this.persons.length) {
+    if (this.numberOfPersons && this.numberOfPersons > this.inputPersons.length) {
       // Add new inputs
-      for (let i = this.persons.length; i < this.numberOfPersons; i++) {
-        this.persons.push({
+      for (let i = this.inputPersons.length; i < this.numberOfPersons; i++) {
+        this.inputPersons.push({
           id: i,
           name: '',
           totalAmount: 0,
           listOfAmounts: [],
         });
       }
-    } else if (this.numberOfPersons && this.numberOfPersons < this.persons.length) {
+    } else if (this.numberOfPersons && this.numberOfPersons < this.inputPersons.length) {
       // Remove extra inputs
-      this.persons.splice(this.numberOfPersons);
+      this.inputPersons.splice(this.numberOfPersons);
     }
-
     this.shouldShowInputs = true;
     this.verifyInputs();
   }
 
   generateBillCards() {
+    this.persons = [...this.inputPersons];
+    this.persons = this.persons.sort((a, b) => a.name.localeCompare(b.name));
     this.shouldShowBillCards = true;
   }
 
   generateAvatar(userName: string): string {
     return `https://avatar.iran.liara.run/public/boy?username=${userName}`;
   }
-
+  capitalizeName(name: string): string {
+    return name.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+  }
   verifyInputs(): void {
-    const names = this.persons.map(person => person.name.trim());
+    this.inputPersons.forEach(person => {
+      person.name = this.capitalizeName(person.name.trim());
+    });
+    const names = this.inputPersons.map(person => person.name);
     const uniqueNames = new Set(names);
     this.enableGenerateCardButton = names.length === uniqueNames.size && names.every(name => name !== '');
   }
-
   addNewAmountInput(id: number): void {
     const person = this.persons.find(p => p.id === id);
     if (person) {
@@ -129,14 +137,18 @@ export class HomeComponent {
           const amountWithGST = (amount.amount ?? 0) + gstAmount;
           return sum + amountWithGST;
         }, 0);
-        this.totalBill += person.totalAmount;
       });
     } else {
       this.persons.forEach(person => {
         person.totalAmount = person.listOfAmounts.reduce((sum, amount) => sum + (amount.amount ?? 0), 0);
-        this.totalBill += person.totalAmount;
       });
     }
+    if (this.discountOnTotalBill && this.discountOnTotalBill > 0) {
+      this.persons.forEach(person => {
+        person.totalAmount -= person.totalAmount * (this.discountOnTotalBill! / 100);
+      });
+    }
+    this.totalBill = this.persons.reduce((sum, person) => sum + person.totalAmount, 0);
   }
 
   resetEverything(): void {
@@ -149,6 +161,9 @@ export class HomeComponent {
     this.totalBill = 0;
     this.selectedPersons = [];
     this.itemPrice = undefined;
+    this.itemDiscount = undefined;
+    this.inputPersons = [];
+    this.discountOnTotalBill = undefined;
   }
 
   selectAllPersons() {
@@ -164,7 +179,9 @@ export class HomeComponent {
             .map(person => [person.id, person])
         ).values()
       );
-      const amountPerPerson = Number((this.itemPrice / this.selectedPersons.length).toFixed(2));
+      let amountPerPerson = Number((this.itemPrice / this.selectedPersons.length).toFixed(2));
+      const discountPercentageOnItem = this.itemDiscount && this.itemDiscount > 0 ? this.itemDiscount : 0;
+      amountPerPerson -= amountPerPerson * (discountPercentageOnItem / 100);
       this.selectedPersons.forEach(person => {
         const personToUpdate = this.persons.find(p => p.id === person.id);
         if (personToUpdate) {
@@ -174,7 +191,16 @@ export class HomeComponent {
       });
       this.totalBill += this.itemPrice;
       this.itemPrice = undefined;
+      this.itemDiscount = undefined;
       this.selectedPersons = []
+    }
+  }
+
+  removeAmountInput(personId: number, amountId: number): void {
+    const person = this.persons.find(p => p.id === personId);
+    if (person) {
+      person.listOfAmounts = person.listOfAmounts.filter(amount => amount.id !== amountId);
+      this.calculateBill();
     }
   }
 }
